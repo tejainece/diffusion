ffbaae51d00080d8d737ee0866f559589518f9d4
Ravi Teja Gudapati <tejainece@gmail.com>
2025-12-06 21:35:04 +0100

    vae, unet

---
+++ example/resnet2d.dart
@@ -0,0 +1,20 @@
+import 'package:tensor/tensor.dart';
+
+void main() {
+  final context = Context.best();
+
+  final generator = Generator.getDefault(device: context.device);
+  generator.currentSeed = 0;
+
+  final sample = Tensor.randn([1, 32, 64, 64], device: context.device);
+  final temb = Tensor.randn([1, 128], device: context.device);
+
+  // Basic block
+  final resnet = ResnetBlock2D.make(
+    numInChannels: 32,
+    numOutChannels: 32,
+    numTembChannels: 128,
+  );
+  final out = resnet.forward(sample, embeds: temb, context: context);
+  print(out);
+}
